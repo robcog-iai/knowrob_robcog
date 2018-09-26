b0d7832654fb431d844f2e1332da7105d66668fa
Björn Veit <bveit@uni-bremen.de>
2018-09-26 22:02:00 +0200

    Added spawn semantic map call to Module.

--- prolog/prolog_service_calls.pl
+++ prolog/prolog_service_calls.pl
@@ -1,9 +1,11 @@
 :- module(prolog_service_calls,
     [
-    spawn_model/9,
+    spawn_model/2,
     spawn_constraint/1,
-    attach_model_to_parent/2
+    attach_model_to_parent/2,
+    spawn_semantic_map/4
     ]).
 
 :- use_foreign_library('libprolog_service_calls.so').
+:- consult('./spawn_semantic_map.pl').
 :- ros_init.
